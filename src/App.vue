036545d5a3097d69e35a03bad6a08f3f07b3a339
13ruceYu <bruceyuhongbo@gmail.com>
2022-12-05 23:36:59 +0800

    feat: uploader basic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="app-container">
+    <UploaderFile action="http://123.57.138.48/api/upload/"></UploaderFile>
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import UploaderFile from './components/UploaderFile.vue'
+
 export default defineComponent({
   name: 'App',
+  components: {
+    UploaderFile,
+  },
 })
 </script>
 
